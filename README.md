b5b7a2e6549272c2009c08afa614dded5de8b79b
Tom Gehder <tom.gehder@leanix.net>
2020-12-10 21:14:55 +0100

    Enviornment is ready for implementation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# sdb-foxx-service
+# hello-world
+
+A simple Hello World Foxx service
+
+## License
+
+The Apache-2.0 license. For more information, see the accompanying LICENSE file.
---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+const createRouter = require("@arangodb/foxx/router");
+
+const router = createRouter();
+module.context.use(router);
+
+router
+  .get("/", (req, res) => {
+    res.write("Hello You!");
+  })
+  .response(["text/plain"]);
